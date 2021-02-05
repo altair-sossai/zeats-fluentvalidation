ac2623610f4a4a0752a0429fa090ebe745779601
Altair Sossai <altairsossai@gmail.com>
2021-02-05 17:10:38 -0300

    Inicio do projeto

---
+++ Zeats.FluentValidation/Validators/UrlValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Zeats.Url;
+
+namespace Zeats.FluentValidation.Validators
+{
+    public static class UrlValidator
+    {
+        public static IRuleBuilderOptions<T, string> Url<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(UrlHelper.IsValidUrl);
+        }
+
+        public static IRuleBuilderOptions<T, string> HttpUrl<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(UrlHelper.IsValidHttpUrl);
+        }
+
+        public static IRuleBuilderOptions<T, string> HttpsUrl<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(UrlHelper.IsValidHttpsUrl);
+        }
+    }
+}
