31795659394434182bf53fea0e303a6d723bd80f
Altair Sossai <altair_sossai@outlook.com>
2022-01-04 09:55:54 -0800

    namespace fix

--- Zeats.FluentValidation.UnitTest/Validators/UrlValidatorTest.cs
+++ Zeats.FluentValidation.UnitTest/Validators/UrlValidatorTest.cs
@@ -2,58 +2,59 @@ using FluentValidation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zeats.FluentValidation.Validators;
 
-namespace Zeats.FluentValidation.UnitTest.Validators;
-
-[TestClass]
-public class UrlValidatorTest
+namespace Zeats.FluentValidation.UnitTest.Validators
 {
-    [TestMethod]
-    public void IsValid()
+    [TestClass]
+    public class UrlValidatorTest
     {
-        var entity = new TestEntity
+        [TestMethod]
+        public void IsValid()
         {
-            Url = "http://www.google.com/",
-            HttpUrl = "http://www.google.com/",
-            HttpsUrl = "https://www.google.com/"
-        };
+            var entity = new TestEntity
+            {
+                Url = "http://www.google.com/",
+                HttpUrl = "http://www.google.com/",
+                HttpsUrl = "https://www.google.com/"
+            };
 
-        var validator = new TestValidator();
-        var result = validator.Validate(entity);
+            var validator = new TestValidator();
+            var result = validator.Validate(entity);
 
-        Assert.IsTrue(result.IsValid);
-    }
+            Assert.IsTrue(result.IsValid);
+        }
 
-    [TestMethod]
-    public void Invalid()
-    {
-        var entity = new TestEntity
+        [TestMethod]
+        public void Invalid()
         {
-            Url = "ftp://www.google.com/",
-            HttpUrl = "https://www.google.com/",
-            HttpsUrl = "http://www.google.com/"
-        };
+            var entity = new TestEntity
+            {
+                Url = "ftp://www.google.com/",
+                HttpUrl = "https://www.google.com/",
+                HttpsUrl = "http://www.google.com/"
+            };
 
-        var validator = new TestValidator();
-        var result = validator.Validate(entity);
+            var validator = new TestValidator();
+            var result = validator.Validate(entity);
 
-        Assert.IsFalse(result.IsValid);
-        Assert.AreEqual(3, result.Errors.Count);
-    }
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(3, result.Errors.Count);
+        }
 
-    public class TestEntity
-    {
-        public string Url { get; set; }
-        public string HttpUrl { get; set; }
-        public string HttpsUrl { get; set; }
-    }
-
-    public class TestValidator : AbstractValidator<TestEntity>
-    {
-        public TestValidator()
+        public class TestEntity
         {
-            RuleFor(r => r.Url).Url();
-            RuleFor(r => r.HttpUrl).HttpUrl();
-            RuleFor(r => r.HttpsUrl).HttpsUrl();
+            public string Url { get; set; }
+            public string HttpUrl { get; set; }
+            public string HttpsUrl { get; set; }
+        }
+
+        public class TestValidator : AbstractValidator<TestEntity>
+        {
+            public TestValidator()
+            {
+                RuleFor(r => r.Url).Url();
+                RuleFor(r => r.HttpUrl).HttpUrl();
+                RuleFor(r => r.HttpsUrl).HttpsUrl();
+            }
         }
     }
 }
--- Zeats.FluentValidation/Validators/UrlValidator.cs
+++ Zeats.FluentValidation/Validators/UrlValidator.cs
@@ -1,22 +1,23 @@
 ﻿using FluentValidation;
 using Zeats.Url;
 
-namespace Zeats.FluentValidation.Validators;
-
-public static class UrlValidator
+namespace Zeats.FluentValidation.Validators
 {
-    public static IRuleBuilderOptions<T, string> Url<T>(this IRuleBuilder<T, string> ruleBuilder)
+    public static class UrlValidator
     {
-        return ruleBuilder.Must(UrlHelper.IsValidUrl);
-    }
+        public static IRuleBuilderOptions<T, string> Url<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(UrlHelper.IsValidUrl);
+        }
 
-    public static IRuleBuilderOptions<T, string> HttpUrl<T>(this IRuleBuilder<T, string> ruleBuilder)
-    {
-        return ruleBuilder.Must(UrlHelper.IsValidHttpUrl);
-    }
+        public static IRuleBuilderOptions<T, string> HttpUrl<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(UrlHelper.IsValidHttpUrl);
+        }
 
-    public static IRuleBuilderOptions<T, string> HttpsUrl<T>(this IRuleBuilder<T, string> ruleBuilder)
-    {
-        return ruleBuilder.Must(UrlHelper.IsValidHttpsUrl);
+        public static IRuleBuilderOptions<T, string> HttpsUrl<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.Must(UrlHelper.IsValidHttpsUrl);
+        }
     }
 }
